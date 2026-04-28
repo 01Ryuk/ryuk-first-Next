d7e6da28cacf399486436ee2279b2639ab72855f
01Ryuk <imoukhuedeemmanuel5@gmail.com>
2026-04-28 08:20:30 +0100

    login auth
    removed session check from the login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,14 @@
 import { auth } from "@/src/lib/auth";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 import LoginClientPage from "./loginclient";
+import DashboardClientPage from "../../dashboard/dashboard-client";
 
-export default async function LoginPage() { 
-const session = await auth.api.getSession({
-  headers: await headers(),
-});
-  if (session) {
-    redirect ("/dashboard");
-  }
+export default async function LoginPage() {
+  // const session = await auth.api.getSession({
+  //   headers: await headers(),
+  // });
+  //     return <DashboardClientPage session={session!} />;
+
+  // no session check needed — proxy handles it
   return <LoginClientPage />;
-};
+}
