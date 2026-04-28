cd3b48f19196d900359ccaab2d92e54995460189
01Ryuk <imoukhuedeemmanuel5@gmail.com>
2026-04-28 08:20:49 +0100

    sign up auth
    removed session check from the signup page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -4,11 +4,11 @@ import { redirect } from "next/navigation";
 import SignupClientPage from "./signupclient";
 
 export default async function SignupPage() { 
-const session = await auth.api.getSession({
-  headers: await headers(),
-});
-  if (session) {
-    redirect ("/dashboard");
-  }
+// const session = await auth.api.getSession({
+//   headers: await headers(),
+// });
+  // if (session) {
+  //   redirect ("/dashboard");
+  // }
   return <SignupClientPage />;
 };
