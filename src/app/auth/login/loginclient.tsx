9534fbddccf5ad850b156218ae3e4c4b03cd4e0b
01Ryuk <imoukhuedeemmanuel5@gmail.com>
2026-04-29 09:52:01 +0100

    login ui update
    added link for the password reset page on the login ui

--- src/app/auth/login/loginclient.tsx
+++ src/app/auth/login/loginclient.tsx
@@ -176,6 +176,12 @@ export default function LoginClientPage() {
             Sign up
           </Link>
         </p>
+        <p className="text-center mt-4 text-gray-600">
+          Forgot Password?
+          <Link href="#"
+            className="text-blue-500 hover:text-blue-700 ml-1">
+          Password Reset</Link>
+        </p>
       </div>
     </div>
   );
