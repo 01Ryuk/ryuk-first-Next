b52c11588f814874f2a12003a310866cf2a883fe
01Ryuk <imoukhuedeemmanuel5@gmail.com>
2026-05-01 08:24:54 +0100

    Update page.tsx

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -1,31 +1,31 @@
 import { createPost } from "@/src/actions/actions";
- export default function CreatePostPage() {
+export default function CreatePostPage() {
   return (
     <div className="text-center pt-12">
       <h1 className="text-3xl capitalize font-bold">Create a new post</h1>
       <form
-             action={createPost}
-             className="flex flex-col gap-y-2 max-w-md mx-auto mt-8 w[400px]"
-           >
-             <input
-               type="text"
-               name="title"
-               placeholder="Title"
-               className="border border-gray-300 rounded-md p-2"
-             />
-             <textarea
-               name="content"
-               rows={5}
-               placeholder="Content"
-               className="border border-gray-300 rounded-md p-2"
-             />
-             <button
-               type="submit"
-               className="bg-blue-500 text-white px-4 py-2 rounded-sm"
-             >
-               Create Post
-             </button>
-           </form>
+        action={createPost}
+        className="flex flex-col gap-y-2 max-w-md mx-auto mt-8 w[400px]"
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="border border-gray-300 rounded-md p-2"
+        />
+        <textarea
+          name="content"
+          rows={5}
+          placeholder="Content"
+          className="border border-gray-300 rounded-md p-2"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-sm"
+        >
+          Create Post
+        </button>
+      </form>
     </div>
   );
 }
